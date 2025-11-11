a6d6472cf4cf9818ab3df055b5c51c0f6ec87aa9
YannTheMan03 <yann.bolton@outlook.com>
2025-11-11 11:42:15 +0000

    added leaderboard class

---
+++ iteration1/LeaderboardForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iteration1
+{
+    public partial class LeaderboardForm : Form
+    {
+        public LeaderboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LeaderboardForm_Load(object sender, EventArgs e)
+        {
+            this.Height = 60;
+            this.Width = 40;
+        }
+    }
+}
